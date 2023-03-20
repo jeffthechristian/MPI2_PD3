c7cc0d08459b3058d128b4332cfcb3fc07eb22b6
Signe Grosberga <signe.grosberga@va.lv>
2023-03-20 21:30:58 +0200

    BigPush
    Needs to be fixed:
    - web view;
    - button (second click closes the weather info);
    - weather  info needs to show also long/lat

---
+++ App.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+const API_KEY = '126c56d5cb7bfcc040bdb11c937105f9';
+
+const App = () => {
+  const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let { coords } = await Location.getCurrentPositionAsync({});
+      setLocation(coords);
+    })();
+  }, []);
+
+  const fetchWeather = async () => {
+    if (!location) {
+      return;
+    }
+
+    const { latitude, longitude } = location;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
+
+    try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      setWeather(data);
+    } catch (error) {
+      console.log('Error fetching weather data', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {location ? (
+        <MapView
+          style={styles.map}
+          showsUserLocation
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker coordinate={{ latitude: location.latitude, longitude: location.longitude }} />
+        </MapView>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+      {weather && (
+        <View style={styles.weatherContainer}>
+          <Text style={styles.weatherText}>
+            {weather.name}: {weather.weather[0].main} ({weather.main.temp}°C)
+          </Text>
+        </View>
+      )}
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.button} onPress={fetchWeather}>
+          <Text style={styles.buttonText}>Show Weather</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+  },
+  header: {
+    position: 'absolute',
+    top: 30,
+    left: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  button: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  weatherContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    padding: 10,
+    borderRadius: 10,
+    minWidth: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  weatherText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#555',
+  },
+});
+
+export default App;
